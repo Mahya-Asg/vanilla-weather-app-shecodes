56bd33f9504c415c74aa6cbd31675f0669ad32a0
mahya-asg <mahya-asg@users.noreply.github.com>
2023-01-15 17:33:45 +0330

    set the date and weather condition to current api info

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
+function formatDate(timestamp) {
+	let date = new Date(timestamp);
+	let hours = date.getHours();
+	if (hours < 10) {
+		hours = `0${hours}`;
+	}
+	let minutes = date.getMinutes();
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wendnesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	let day = days[date.getDay()];
+	return `${day} ${hours}:${minutes}`;
+}
+
 function changeInnerHTML(idName, newContent) {
 	let selectedElement = document.querySelector(idName);
 	selectedElement.innerHTML = newContent;
 }
 function displayApiData(response) {
-	console.log(response.data.temperature.current);
+	console.log(response.data);
+	// change date
+	changeInnerHTML(".date", formatDate(response.data.time));
+  changeInnerHTML(".weather-description", response.data.condition.description)
 	// change temperature degree
 	changeInnerHTML(".degree", Math.round(response.data.temperature.current));
 	// change humidity
@@ -11,7 +37,10 @@ function displayApiData(response) {
 	//  change wind speed
 	changeInnerHTML(".wind", Math.round(response.data.wind.speed));
 	//  change feels like
-	changeInnerHTML(".feelsLike", Math.round(response.data.temperature.feels_like));
+	changeInnerHTML(
+		".feelsLike",
+		Math.round(response.data.temperature.feels_like)
+	);
 }
 let key = "9080739tbf37e964oc44a735390ad04b";
 let query = "tehran";
