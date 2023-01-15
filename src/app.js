73ee66e9561a8fab8d5e9c3849c4232e6b1d5d00
mahya-asg <mahya-asg@users.noreply.github.com>
2023-01-15 14:23:07 +0330

    update weather conditions innerHTML (temp, humidity, ...) based on the given api query

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-// alert('well done!')
+function changeInnerHTML(idName, newContent) {
+	let selectedElement = document.querySelector(idName);
+	selectedElement.innerHTML = newContent;
+}
+function displayApiData(response) {
+	console.log(response.data.temperature.current);
+	// change temperature degree
+	changeInnerHTML(".degree", Math.round(response.data.temperature.current));
+	// change humidity
+	changeInnerHTML(".humidity", response.data.temperature.humidity);
+	//  change wind speed
+	changeInnerHTML(".wind", Math.round(response.data.wind.speed));
+	//  change feels like
+	changeInnerHTML(".feelsLike", Math.round(response.data.temperature.feels_like));
+}
+let key = "9080739tbf37e964oc44a735390ad04b";
+let query = "tehran";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}&units=metric`;
+
+axios.get(apiUrl).then(displayApiData);
